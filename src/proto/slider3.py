15705886dd6309ebf412ed7b1e4f449240376a04
Roy <11579416+roynielsen17@users.noreply.github.com>
2025-03-20 21:17:39 -0600

    attempted beginning of ui - messy.

---
+++ src/proto/slider3.py
@@ -0,0 +1,57 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QSlider, QLineEdit, QLabel
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QIntValidator
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Slider with Editable Line Edit")
+
+        # Create a central widget and a layout
+        central_widget = QWidget()
+        layout = QVBoxLayout()
+
+        # Create a label
+        self.label = QLabel("Value: 0")
+        layout.addWidget(self.label)
+
+        # Create a slider
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setRange(0, 100)
+        layout.addWidget(self.slider)
+
+        # Create an editable line edit
+        self.line_edit = QLineEdit()
+        self.line_edit.setValidator(QIntValidator(0, 100))  # Ensure the input is within the slider's range
+        layout.addWidget(self.line_edit)
+
+        # Connect the slider's valueChanged signal to the update_line_edit method
+        self.slider.valueChanged.connect(self.update_line_edit)
+
+        # Connect the line edit's textChanged signal to the update_slider method
+        self.line_edit.textChanged.connect(self.update_slider)
+
+        # Set the central widget and layout
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def update_line_edit(self, value):
+        self.line_edit.setText(str(value))
+        self.label.setText(f"Value: {value}")
+
+    def update_slider(self, text):
+        try:
+            value = int(text)
+            self.slider.setValue(value)
+            self.label.setText(f"Value: {value}")
+        except ValueError:
+            pass  # Ignore invalid input
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
---
+++ src/ramdisk_widget/main.py
@@ -0,0 +1,165 @@
+
+import datetime
+import logging
+
+from PySide6.QtCore import QCoreApplication, QEvent, QSize, Qt, Slot
+from PySide6.QtGui import QCursor, QDragMoveEvent, QDropEvent, QFont
+from PySide6.QtWidgets import (QAbstractItemView, QAbstractScrollArea,
+                               QApplication, QFrame, QLabel, QListWidget,
+                               QListWidgetItem, QMainWindow, QPushButton,
+                               QSizePolicy, QSpacerItem, QVBoxLayout, QWidget,
+                               QMessageBox, QDialog, QDialogButtonBox)
+
+
+import sys 
+# from PySide6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QVBoxLayout, QLabel, QDialogButtonBox
+# from PySide6.QtWidgets import QMessageBox
+from PySide6.QtGui import QIntValidator
+
+
+from ui_main import Ui_MainWindow
+from ui_not_yet_implemented import Ui_Dialog
+
+
+
+class CustomDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Oops")
+
+        self.button = QPushButton("Close")
+        self.button.clicked.connect(self.accept)
+        layout = QVBoxLayout()
+        message = QLabel("Not Yet Implemented")
+        layout.addWidget(message)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+
+class _CreateRamdisk(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        '''   
+    def __init__(self, parent: QMainWindow) -> None:
+        QMainWindow.__init__(self)
+        
+    def __init__(self):
+        super().__init__()
+        '''
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        #####
+        # Connect Button click signals to slots 
+        self.ui.createPushButton.clicked.connect(
+               lambda: self.notYetImplemented())
+        self.ui.debugPushButton.clicked.connect(
+               lambda: self.notYetImplemented())
+        self.ui.quitPushButton.clicked.connect(
+               lambda: self.quit_application())
+        self.ui.rListPushButton.clicked.connect(
+               lambda: self.notYetImplemented())
+    
+        #####
+        # Connect Button click signals to slots 
+        self.ui.createPushButton.keyPressEvent = lambda event: keyPressEvent(event, parent, self.notYetImplemented()) 
+        self.ui.debugPushButton.keyPressEvent = lambda event: keyPressEvent(event, parent, self.notYetImplemented())
+        self.ui.quitPushButton.keyPressEvent = lambda event: keyPressEvent(event, parent, self.quit_application())
+        self.ui.rListPushButton.keyPressEvent = lambda event: keyPressEvent(event, parent, self.notYetImplemented())
+
+        #####
+        # connect slider to line edit
+        self.ui.sizeHorizontalSlider.valueChanged.connect(self.update_line_edit)
+
+        #####
+        # connect line edit to slider
+        
+        # CEnsure the inputs within the slider's range
+        self.ui.sizeLineEdit.setValidator(QIntValidator(0, 100))
+        self.ui.sizeLineEdit.textChanged.connect(self.update_slider)
+
+        print("exiting init...")
+
+    def update_slider(self, text):
+        try:
+            value = int(text)
+            self.ui.sizeHorizontalSlider.setValue(value)
+        except ValueError:
+            pass   
+
+    def update_line_edit(self, value):
+        self.ui.sizeLineEdit.setText(str(value) + "M")
+
+    def quit_application(self):
+        self.close()
+
+    def closeEvent(self, event):
+        print("Entered the twilight zone....")
+        # Perform any necessary cleanup or confirmation actions here
+        reply = QMessageBox.question(self, 'Message', 'Are you sure you want to quit?', 
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+        if reply == QMessageBox.Yes:
+            event.accept()  # Let the window close
+        else:
+            event.ignore()  # Prevent the window from closing
+        #event.accept()  # Let the window close
+        print("Exiting the twilight zone!!!")        
+
+    def notYetImplemented(self):
+        """
+        pop up a window indicating that this functionality is not yet 
+        implemented...
+        """
+        # print("click", s)
+
+        dlg = CustomDialog()
+        dlg.show()
+        dlg.raise_()
+        if dlg.exec():
+            print("User clicked OK")
+
+
+'''
+def init_event_logger(path: str, fmt: str, debug: bool = False,
+                      stdout: bool = False) -> None:
+    """Initializes the event logger.
+    - Set the path of the event log file
+    - Set the format of the event log file
+    - Set the debug level of the event log file
+    - Set up the event logger
+    """
+    logging.basicConfig(
+        filename=path,
+        filemode="w",
+        format=fmt,
+        datefmt="%d-%b-%y %H:%M:%S",
+        level=logging.DEBUG if debug else logging.INFO,
+    )
+    if stdout:
+        logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+'''
+
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    """
+    # Set up event logger
+    init_event_logger(
+        os.path.join(get_current_directory(), "event.log"),
+        "%(asctime)s - %(levelname)s - %(message)s",
+        stdout=True,
+        debug=True if "--debug" in sys.argv else False,
+    )
+    """
+    print("started app...")
+    window = _CreateRamdisk()
+    print("initiated window")
+    window.show()
+    print("showing window...")
+    window.raise_()
+    print("raising_ window")
+    sys.exit(app.exec())
+
+
