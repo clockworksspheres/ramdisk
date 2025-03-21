bd0b37d41c51e1302ea2ea298faa3c1cedc5f491
Roy <11579416+roynielsen17@users.noreply.github.com>
2025-03-20 21:57:50 -0600

    attempted fix to not_yet_implemented

---
+++ src/ramdisk_widget/ui_not_yet_implemented.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'not_yet_implemented.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QPushButton,
+    QSizePolicy, QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(275, 118)
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(90, 70, 100, 32))
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(20, 20, 231, 41))
+        font = QFont()
+        font.setFamilies([u"Arimo"])
+        font.setPointSize(24)
+        self.label.setFont(font)
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"ok", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"Not Yet Implemented", None))
+    # retranslateUi
+
