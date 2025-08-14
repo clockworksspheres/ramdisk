010a48e0c6dc359acb30218146d764a9e7f3755d
Roy <11579416+roynielsen17@users.noreply.github.com>
2025-08-14 17:52:28 -0600

    adding build support for windows, need to revise resources to eliminate eisenban realted files

---
+++ src/build.windows.ps1
@@ -0,0 +1,35 @@
+# highly modified version of:
+# https://www.pythonguis.com/tutorials/packaging-pyside6-applications-windows-pyinstaller-installforge/
+# amoung others... including
+# https://pyinstaller.org/en/stable/
+
+# before script is run:
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+# powershell -File ".\eisenban.windows.ps1"
+
+#if doesn't exist...
+# cd to the eisenban source root
+
+$FolderPath = ".\packenv"
+if (!(Test-Path -Path $FolderPath -PathType Container)) {
+   
+   python3 -m venv packenv
+   .\packenv\Scripts\Activate.ps1
+
+   pip3 install PySide6 PyInstaller packaging pywin32
+   pip3 install --upgrade PyInstaller pyinstaller-hooks-contrib
+
+} else {
+    .\packenv\Scripts\Activate.ps1
+}
+
+#####
+# Do every time, to make sure everyone knows source of E.ico icon, so 
+# proper license can be found
+# cp .\resources\icons\Barkerbaggies-Bag-O-Tiles-E.ico .\resources\icons\E.ico
+
+pyinstaller --clean -y build.windows11.onefile.spec
+pyinstaller -y build.windows11.onefile.spec
+
+
+
