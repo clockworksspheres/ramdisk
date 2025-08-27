d77a304c5a36899faae63eda553950675f6a6c39
Roy <roynielsen17@gmail.com>
2025-08-27 12:32:22 -0600

    rh-based scripts prototyped

--- src/buildScripts/build.rh-based.sh
+++ src/buildScripts/build.rh-based.sh
@@ -5,6 +5,8 @@
 # amoung others... including
 # https://pyinstaller.org/en/stable/
 
+pushd ..
+
 #if doesn't the packenv directory doesn't exist...
 
 directory="./packenv"
@@ -12,18 +14,21 @@ actfile="./packenv/bin/activate"
 if [ ! -d "$directory" ]  || [ ! -f "$actfile" ] ; then
    python3 -m venv packenv
 
+   source packenv/bin/activate
+
+   pip install --upgrade pip
    pip3 install PySide6 PyInstaller
    pip3 install --upgrade PyInstaller pyinstaller-hooks-contrib
    pip3 install packaging
+else
+   source packenv/bin/activate
 fi
-source packenv/bin/activate
 
-pushd ..
+cp buildScripts/build.rh-based.py313.onefile.spec .
 
-pyinstaller --clean -y build.ubuntu2024.py312.onefile.spec
-pyinstaller -y build.ubuntu2024.py312.onefile.spec
-./dist/ramdisk-setup
+pyinstaller --clean -y build.rh-based.py313.onefile.spec
+pyinstaller -y build.rh-based.py313.onefile.spec
+rm build.rh-based.py313.onefile.spec
 popd
-
-
+../dist/ramdisk-setup
 
