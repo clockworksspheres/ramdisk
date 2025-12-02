30589058c0c3213f243011bf67bcbcfa1afefc14
Roy Nielsen <roy.nielsen17@gmail.com>
2025-12-02 11:51:18 -0700

    changing algo for printing mountpoint and device

--- src/proto/mntpoints/aimgetmntpnts.py
+++ src/proto/mntpoints/aimgetmntpnts.py
@@ -3,9 +3,19 @@ import re
 
 result = subprocess.run(['aim_ll', '-l'], capture_output=True, text=True)
 for line in result.stdout.splitlines():
+    line = line.strip()
     if re.search("\\\\\\\\", line):
         continue
     elif re.search("000", line):
-        print(line)
+        # print(line)
+        amchor = True
+        device = line.split()[-1]
     elif re.search(':\\\\.*', line):
-        print(line)
+        # print(line)
+        mountname = line.split()[-1]
+    elif not line:
+        anchor = False
+        print(f"{mountname} : {device}")
+        continue
+
+     
