7fd5c50c351b04e0deb7f7eac14273f944848b25
Roy Nielsen <roy.nielsen17@gmail.com>
2026-02-10 09:56:53 -0700

    attempting fixes to make tests work on Windows and the ubuntu 2404 build work on WSL

--- src/tests/test_fsHelper.py
+++ src/tests/test_fsHelper.py
@@ -13,8 +13,9 @@ from datetime import datetime
 
 #####
 # Include the parent project directory in the PYTHONPATH
-appendDir = "/".join(os.path.abspath(os.path.dirname(__file__)).split('/')[:-1])
-sys.path.append(appendDir)
+# appendDir = "/".join(os.path.abspath(os.path.dirname(__file__)).split('/')[:-1])
+# sys.path.append(appendDir)
+sys.path.append("..")
 
 #--- non-native python libraries in this source tree
 from ramdisk.lib.loggers import CyLogger
