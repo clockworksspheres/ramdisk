ad2154835c401d5281e34b8f8839f29f82e17a56
Roy <roynielsen17@gmail.com>
2026-02-14 14:58:10 -0700

    updating stubs

---
+++ src/BuildScripts/jenkinsScripts/JenkinsTools/NodeManage.py
@@ -0,0 +1,33 @@
+
+
+class NodeManage():
+    def __init__(self, args):
+        self.args = args
+        print(f"Initializing {self.__class__.__name__} class")
+
+    def delete_node(self):
+        """
+        """
+        pass
+
+    def disable_node(self):
+        """
+        """
+        pass
+
+    def enable_node(self):
+        """
+        """
+        pass
+
+    def add_node(self):
+        """
+        """
+        pass
+
+    def update_node(self):
+        """
+        """
+        pass
+
+
---
+++ src/BuildScripts/jenkinsScripts/JenkinsTools/NodeStatus.py
@@ -0,0 +1,33 @@
+
+
+class NodeStatus():
+    def __init__(self, args):
+        self.args = args
+        print(f"Initializing {self.__class__.__name__} class")
+
+    def assert_node_exists(self):
+        """
+        """
+        pass
+
+    def node_exists(self):
+        """
+        """
+        pass
+
+    def get_nodes(self):
+        """
+        """
+        pass
+
+    def get_node_info
+        """
+        """
+        pass
+
+    def get_node_config(self):
+        """
+        """
+        pass
+
+
