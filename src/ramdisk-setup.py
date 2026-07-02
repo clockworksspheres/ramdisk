484ced2eeff63b0ffeac44f509a2537fbd26f5c8
Roy Nielsen <roy.nielsen17@gmail.com>
2026-07-02 14:27:42 -0500

    set dark mode on hard for testing

--- src/ramdisk-setup.py
+++ src/ramdisk-setup.py
@@ -65,7 +65,7 @@ if opts.gui:
 
     def set_dark_palette(app):
         dark_palette = QPalette()
-        dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.Window, QColor(106, 106, 106))
         dark_palette.setColor(QPalette.WindowText, Qt.white)
         dark_palette.setColor(QPalette.Base, QColor(42, 42, 42))
         dark_palette.setColor(QPalette.Text, Qt.white)
@@ -97,10 +97,11 @@ if opts.gui:
     if sys.platform.lower().startswith("win"):
         app.setStyle("Fusion") # Required for dark mode on Windows
 
-    if is_night():
-        set_dark_palette(app)
-    else:
-        set_light_palette(app)
+    # if is_night():
+    #     set_dark_palette(app)
+    # else:
+    #     set_light_palette(app)
+    set_dark_palette(app)
 
     print("started app...")
     window = _CreateRamdisk()
